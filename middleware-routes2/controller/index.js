b918c73d936994af62c38f3e098d487fb7d700a3
JosueSdev <josue.sandoval@bigcommerce.com>
2023-07-12 15:02:17 -0600

    add required pages

--- middleware-routes2/controller/index.js
+++ middleware-routes2/controller/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const path = require('node:path');
+
 const router = express.Router();
 
 router.get('/home', function(_req, res) {
-  res.send('Welcome.');
+  res.sendFile(path.join(process.cwd(), 'view/home.html'));
+});
+
+router.route('/login')
+  .get(function(_req, res) {
+    res.sendFile(path.join(process.cwd(), 'view/login.html'));
+  });
+
+router.get('/menu', function(_req, res) {
+  res.sendFile(path.join(process.cwd(), 'view/menu.html'));
+});
+
+router.post('/logout', function (_req, res) {
+  res.sendStatus(200);
 });
 
 module.exports = router;
--- middleware-routes2/readme.md
+++ middleware-routes2/readme.md
@@ -3,7 +3,7 @@
 Requirements:
 
 - [x] Install the express-session middleware
-- [ ] Create an app with /home, /login and /menu pages
+- [x] Create an app with /home, /login and /menu pages
 - [ ] Use express-session to track an user's login
 - [ ] Only allow logged users to see the /menu page
 - [ ] Match any route starting with /menu or /dashboard to the home page
